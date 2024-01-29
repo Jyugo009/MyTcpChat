0e17132f0de203cf8ef106f6e7c7a8af920d3c3d
nekrass06@gmail.com <nekrass06@gmail.com>
2024-01-30 00:25:44 +0200

    done homework

---
+++ MyTcpChat.Server/ClientInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTcpChat.Server
+{
+    public class ClientInfo
+    {
+        public TcpClient TcpClient { get; set; }
+        public string Id { get; private set; }
+        public User User { get; private set; }
+        public bool IsAuthenticated => User != null;
+
+        public ClientInfo(TcpClient tcpClient)
+        {
+            TcpClient = tcpClient;
+            Id = tcpClient.Client.RemoteEndPoint.ToString();
+            User = null;
+        }
+
+        public void Authenticate(User user)
+        {
+            User = user;
+        }
+    }
+}
---
+++ MyTcpChat.Server/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace MyTcpChat.Server
+{
+    public class User
+    {
+        public string Username { get; }
+        public string PasswordHash { get; }
+
+        public User(string username, string password)
+        {
+            Username = username;
+            PasswordHash = HashPassword(password);
+        }
+
+        private static string HashPassword(string password)
+        {
+            using SHA256 sha256 = SHA256.Create();
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+            StringBuilder builder = new StringBuilder();
+
+            foreach (byte b in bytes)
+                builder.Append(b.ToString("x2"));
+
+            return builder.ToString();
+        }
+
+        public bool VerifyPassword(string passwordToVerify)
+        {
+            return PasswordHash == HashPassword(passwordToVerify);
+        }
+    }
+}
